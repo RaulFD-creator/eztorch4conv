2474e3267369f379ad7b96e99919f5adf0eccfc1
RaulFD-creator <raulfdz9@gmail.com>
2022-03-17 16:42:43 +0100

    Some changes

--- eztorch4conv/eztorch4conv.py
+++ eztorch4conv/eztorch4conv.py
@@ -4,10 +4,7 @@ Docustring
 import sys
 import torch
 import torch.nn as nn
-import pandas as pd
 import os
-import random
-from torch.utils.data import Dataset
 import json
 import sys
 
@@ -32,7 +29,8 @@ def parse_inputs(path_to_arguments):
                         "num_classes": Number of different categories that the data is separated into, 
                         "num_channels": Number of channels used,
                         "dataset_proportion": ,
-                        "device":
+                        "device": CPU ('cpu') or GPU ('cuda'). By default, it will check if there is GPU
+                                    available and use it; if not available, will use CPU.
     """
     path_to_arguments = sys.argv[1]
 
@@ -72,12 +70,19 @@ class dense():
     def build_layer(self):
             return nn.Sequential(self.linear, self.leaky_relu, self.dropout)
 
+class flatten():
+    def __init__(self, x):
+        self.x = x
+    def build_layer(self):
+        return nn.Sequential(self.x.view(self.x.size(0), -1))
+
+
 class DCNN(nn.Module):
     def __init__(self):
 
         super(DCNN, self).__init__()
-        self.conv_layers = nn.ModuleList()
-        self.dense_layers = nn.ModuleList()
+        self.layers = nn.ModuleList()
+
 
         self.count = 0
         self.loss_list = []
@@ -86,64 +91,29 @@ class DCNN(nn.Module):
         self.loss_val_list = []
         self.iteration_list = []
         self.accuracy_val_list = []
-
-        #self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        #self.device = "cpu"
         self.float()
-        #self.to(self.device)
     
-    def add_conv_layer(self, other):
-        self.conv_layers.append(other)
+    def add_layer(self, other):
+        self.layers.append(other.build_layer())
     
-    def add_dense_layer(self, other):
-        self.dense_layers.append(other)
-    
-    def add_loss(self, other):
+    def define_loss(self, other):
         self.error = other
 
-    def add_optimizer(self, other):
+    def define_optimizer(self, other):
         self.optimizer = other
 
-    def Flatten(self, x):
-        return x.view(x.size(0), -1)
-    
     def forward(self, x):
         
-        for layer in self.conv_layers:
-            x = layer(x)
-
-        x = self.Flatten(x)
-
-        for layer in self.dense_layers:
+        for layer in self.layers:
             x = layer(x)
         return x
 
-    def save_model(self, accuracy_val):
-        current_number = 0
-        for file in os.listdir("./Models"):
-            try:
-                pointer = file.split("_")[1]
-                pointer = int(pointer.split(".")[0])
-            except:
-                pointer = 0
-                pass
-            if pointer > current_number:
-                current_number = pointer
-        new_number = current_number + 1
-        with open("./Models/log.txt", "a") as fo:
-            fo.write(f"model_{new_number};{accuracy_val}\n")
-            fo.write(f"Input_parameters: {self.input_parameters}")
-        print(f"Saving model as: ./Models/model_{new_number}.pt")
-        torch.save(self, f"./Models/model_{new_number}.pt")
-        print("Model saved.")
+    def save_model(self, filename):
+        torch.save(self, filename)
 
     def train(self, train_dataloader, validate_dataloader, len_training, 
-                epochs, batch_size, model_name, input_parameters):
-        self.input_parameters = input_parameters
+                epochs, batch_size):
         print(f"Training Model using device: {self.device}")
-        with open(f"./Training_logs/dcnn_{model_name}.txt","a") as fo:
-            fo.write(f"Input parameters: {input_parameters}\n")
-
 
         self.num_epochs = epochs
         self.batch_size = batch_size
@@ -166,9 +136,7 @@ class DCNN(nn.Module):
                 self.count += 1
 
                 print(f"Epoch {epoch}/{self.num_epochs}: Batch {i}/{len_training//self.batch_size} \tLoss: {loss.data}")
-                os.system(f"touch ./Training_logs/dcnn_{model_name}.txt")
-                with open(f"./Training_logs/dcnn_{model_name}.txt","a") as fo:
-                    fo.write(f"Epoch {epoch}/{self.num_epochs}: Batch {i}/{len_training//self.batch_size} \tLoss: {loss.data}\n")
+
             # Calculate Accuracy         
             correct = 0
             total = 0
@@ -196,11 +164,8 @@ class DCNN(nn.Module):
                 self.accuracy_list.append(accuracy)
 
             # Print Loss
-            print('Loss training: {}  Validation accuracy: {} %'
-                .format(loss.data, accuracy))
-            with open(f"./Training_logs/dcnn_{model_name}.txt","a") as fo:
-                    fo.write(f"Loss training: {loss.data} \tValidation accuracy: {accuracy}\n")
-            
+            print(f"Loss training: {loss.data}  Validation accuracy: {accuracy}")
+
             if accuracy > 80 and self.count > 10:
                 # Callback
                 self.save_model(accuracy)
@@ -212,13 +177,13 @@ class Channel(nn.Module):
 
         super(Channel, self).__init__()
 
-        self.conv_layers = nn.ModuleList()
+        self.layers = nn.ModuleList()
 
-    def add_conv_layer(self, other):
-        self.conv_layers.append(other)
+    def add_layer(self, other):
+        self.layers.append(other.build_layer())
 
     def forward(self, x):
-        for layer in self.conv_layers:
+        for layer in self.layers:
             x = layer(x)
         return x
 
@@ -237,51 +202,28 @@ class MCDCNN(DCNN):
         self.accuracy_val_list = []
         self.channels = nn.ModuleList()
         self.n_channels = n_channels
-        self.dense_layers = nn.ModuleList()
+        self.layers = nn.ModuleList()
 
-        #self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        #self.device = "cpu"
         self.float()
-        #self.to(self.device)
 
         for _ in range(self.n_channels):
             self.channels.extend([Channel()])
-        
-        for channel in self.channels:
-            channel.add_conv_layer(conv3d(in_channels=1, out_channels=48, conv_kernel=5, 
-                        pooling_type='max', dropout=0.25).build_layer())
-            channel.add_conv_layer(conv3d(in_channels=48, out_channels=64, conv_kernel=5, conv_padding=1,
-                            pooling_type='max', dropout=0.25).build_layer())
-            channel.add_conv_layer(conv3d(in_channels=64, out_channels=96, conv_kernel=3, conv_padding=1,
-                        pooling_type='max', dropout=0.25).build_layer())
+    
+    def add_layer_to_channels(self, channels, layer):
+        for channel in channels:
+            channel.add_layer(layer)
+    
+    def add_layer(self, layer):
+        self.layers.append(layer.build_layer())
     
 
-    def save_model(self, accuracy_val):
-        current_number = 0
-        for file in os.listdir("./MC_Models"):
-            try:
-                pointer = file.split("_")[1]
-                pointer = int(pointer.split(".")[0])
-            except:
-                pointer = 0
-                pass
-            if pointer > current_number:
-                current_number = pointer
-        new_number = current_number + 1
-        with open("./MC_Models/log.txt", "a") as fo:
-            fo.write(f"model_{new_number};{accuracy_val}\n")
-            fo.write(f"Input_parameters: {self.input_parameters}")
-        print(f"Saving model as: ./MC_Models/model_{new_number}.pt")
-        torch.save(self, f"./MC_Models/model_{new_number}.pt")
-        print("Model saved.")
-    
+    def save_model(self, filename):
+        torch.save(self, filename)
     
     def forward(self, x):
         outs = []
         for i, channel in enumerate(self.channels):
-
             out = channel(x[:,i,:,:,:].view(len(x),1,16,16,16))
-            out = self.Flatten(out)
             outs.append(out)
 
         n = out.size()[-1] * len(outs)
@@ -289,15 +231,11 @@ class MCDCNN(DCNN):
         x = torch.cat(outs,dim=0)
         x = x.view(-1,n)
 
-        for layer in self.dense_layers:
+        for layer in self.layers:
             x = layer(x)
-
         return x
-
-
-
 
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
-    print(canvas())
+    print("Hello world")
