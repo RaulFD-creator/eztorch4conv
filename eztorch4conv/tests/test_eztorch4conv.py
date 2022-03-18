67ee858cd49514fbdcfd89f8c1d61ede87aa5d16
Raúl Fernández Díaz <78709417+RaulFD-creator@users.noreply.github.com>
2022-03-18 12:54:25 +0100

    Update test_eztorch4conv.py

--- eztorch4conv/tests/test_eztorch4conv.py
+++ eztorch4conv/tests/test_eztorch4conv.py
@@ -18,7 +18,7 @@ def test_eztorch4conv_imported():
     model.add_layer(conv3d(1,2,3))
     model.add_layer(flatten())
     model.add_layer(dense(40, 2))
-    model2.add_layer_to(conv3d(1,2,3), channels="all")
+    model2.add_layer_to_channels(conv3d(1,2,3), channels="all")
     model2.add_layer(flatten())
     model2.add_layer(dense(40, 2))
 
