459b8e713eaec9453c2a713400b3aa259c04e35e
RaulFD-creator <raulfdz9@gmail.com>
2022-03-25 10:58:18 +0100

    Attempt 3 to fix callback bug

--- eztorch4conv/tests/test_eztorch4conv.py
+++ eztorch4conv/tests/test_eztorch4conv.py
@@ -7,19 +7,19 @@ import sys
 
 import pytest
 
-import eztorch4conv
+#import eztorch4conv
 from eztorch4conv import *
 
 def test_eztorch4conv_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "eztorch4conv" in sys.modules
     model = DCNN("model1", "./")
-    model.add_layers(conv3d(neurons=2, input_shape=(3,9,9,9), conv_kernel=3),
+    model.add_layers([conv3d(neurons=2, input_shape=(3,9,9,9), conv_kernel=3),
                     flatten(),
-                    dense(40))
-    model.add_callbacks(
+                    dense(40)])
+    model.add_callbacks([
                     early_stop(metric=["accuracy"],target=4),
-                    checkpoint(metric=["accuracy"],target=4))
+                    checkpoint(metric=["accuracy"],target=4)])
     
     model2 = MCDCNN(3,"model2", "./")
     model2.add_layer_to_channels(layer=conv3d(neurons=2, input_shape=(3,9,9,9), conv_kernel=3),
