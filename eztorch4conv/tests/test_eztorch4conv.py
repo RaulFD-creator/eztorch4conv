3a8cfdabbd8584f5bc647d7154e7f8d3151a5930
Raúl Fernández Díaz <78709417+RaulFD-creator@users.noreply.github.com>
2022-03-30 22:21:26 +0200

    Update test_eztorch4conv.py

--- eztorch4conv/tests/test_eztorch4conv.py
+++ eztorch4conv/tests/test_eztorch4conv.py
@@ -22,7 +22,7 @@ def test_eztorch4conv_imported():
                     checkpoint(metric=["accuracy"],target=4)])
     
     model2 = MCDCNN("model2", "./", 3)
-    model2.add_layers_to_channels(layer=conv3d(neurons=2, input_shape=(3,9,9,9), conv_kernel=3),
+    model2.add_layers_to_channels([conv3d(neurons=2, input_shape=(3,9,9,9), conv_kernel=3)],
                                  channels="all")
     model2.add_layers([flatten()])
  
