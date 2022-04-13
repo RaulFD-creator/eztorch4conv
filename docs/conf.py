69f4b1886d0612ff921d6eaaef58dc7ab0835f0d
Raúl Fernández Díaz <78709417+RaulFD-creator@users.noreply.github.com>
2022-04-13 11:15:19 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -50,12 +50,11 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
     'sphinx.ext.extlinks',
+    'autodocsumm'
 ]
 
 autosummary_generate = True
-napoleon_google_docstring = False
-napoleon_use_param = False
-napoleon_use_ivar = True
+autodoc_inherit_docstrings = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
